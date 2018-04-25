26cf6cc72cc930f93ce6e3d92baea1a64f6116b2
AveYo <aveyo@yahoo.com>
2018-04-25 10:45:43 +0300

    Update res2batch.bat
    new in v3:  launch from SendTo context menu entry, supports both single files and folders - preserving directory structure!

--- res2batch.bat
+++ res2batch.bat
@@ -1,31 +1,52 @@
-/* 2>nul & TITLE Res2Batch by AveYo v2.0a
-@echo off &setlocal &set "app=%APPDATA%\AveYo"
-md "%app%" >nul 2>&1 &if exist "%~1" (set "fchoice=%~1") else call :filechoice
-cscript.exe //e:javascript //nologo "%~f0" %fchoicejs%
-del /f /q "%fpath%\%fname%%cfext%" &popd &echo. &pause &endlocal &exit /b
-
-goto :eof
-:filechoice
-set "appjs=%app:\=\\%" &set "openpath=HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32"
-reg copy "%openpath%" "%openpath%_bak" /s /f >nul 2>&1 &reg delete "%openpath%" /f >nul 2>&1
-mshta.exe "about:<input type=file style="display:none" id=FILE><script>resizeTo(0,0);FILE.click();var out=new ActiveXObject('Scripting.FileSystemObject').CreateTextFile('%appjs%\\fchoice.bat',true);out.write('@set fchoice='+FILE.value);out.close();close();</script>"
-reg copy "%openpath%_bak" "%openpath%" /s /f >nul 2>&1 &reg delete "%openpath%_bak" /f >nul 2>&1
-call "%app%\fchoice.bat" &del /f /q "%app%\fchoice.bat"
-rem needs compression?
-for %%A in ("%fchoice%") do set "fpath=%%~dpA" &set "fname=%%~nA" &set "fext=%%~xA" &set "fsize=%%~zA"
-if %fsize% GTR 10485760 echo ERROR! Chosen file is larger than 10MB, what are you trying to do?! &pause &exit
-rem comment above line to remove 10MB file limit
-set "cfext=%fext:~0,-1%_"
-pushd "%fpath%" &makecab.exe "%fchoice%" "%fname%%cfext%" >nul 2>&1
-for %%A in ("%fname%%cfext%") do set "cfsize=%%~zA"
-if %cfsize% LSS %fsize% set "fchoice="%fpath%\%fname%%cfext%"
-set fchoicejs=%fchoice: =_RSPACE_%
-goto :eof
-
+@set @v=3 /* AveYo: Efficient file or folder to .bat converter via makecab compression and optimized res85 ascii encoder 
+@echo off &title File2Bach [%1] &if not ".%1"=="." setlocal &goto :Encode
+echo.&echo FILE2BACH: No input file or folder to encode! use 'Send to' context menu ...
+copy /y "%~f0" "%APPDATA%\Microsoft\Windows\SendTo\File2Bach.bat" >nul 2>nul &goto :End
+:Encode
+call :MakeCab "%~f1" &if not exist "%~dpn1.cab" echo.&echo FILE2BACH: makecab %~nx1 failed, try again ... &goto :End 
+for %%# in ("%~dpn1.cab") do set "fsize=%%~z#" &set "finput=%%~dpn#"
+if 1%fsize% GTR 110485760 echo.&echo FILE2BACH: Input %fname% is larger than 10MB, what are you trying to do?! &goto :End
+::rem comment above line to remove 10MB file limit 
+cscript.exe //nologo //e:JScript "%~f0" "%finput: =_RSPACE_%.cab"
+if exist "%finput%.cab.bat" del /f /q "%finput%.cab" >nul 2>nul &echo.&echo DONE!
+:End
+ping -n 6 localhost >nul &title %comspec% &endlocal &exit/b
+:MakeCab %1:[file or directory]
+pushd %~dp1 &set "InputFile=yes" &for /f "tokens=1 delims=r-" %%# in ("%~a1") do if /i ".%%#"==".d" set "InputFile=" 
+if defined InputFile makecab.exe /D CompressionType=LZX /D CompressionLevel=7 /D CompressionMemory=21 "%~nx1" "%~n1.cab" &exit/b
+::dir /a:-D/b/s "%~1"
+set ddf="%temp%\ddf"
+ >%ddf% echo/.New Cabinet
+>>%ddf% echo/.set Cabinet=ON
+>>%ddf% echo/.set CabinetFileCountThreshold=0
+>>%ddf% echo/.set Compress=ON
+>>%ddf% echo/.set CompressionType=LZX
+>>%ddf% echo/.set CompressionLevel=7
+>>%ddf% echo/.set CompressionMemory=21
+>>%ddf% echo/.set FolderFileCountThreshold=0
+>>%ddf% echo/.set FolderSizeThreshold=0
+>>%ddf% echo/.set GenerateInf=OFF
+>>%ddf% echo/.set InfFileName=nul
+>>%ddf% echo/.set MaxCabinetSize=0
+>>%ddf% echo/.set MaxDiskFileCount=0
+>>%ddf% echo/.set MaxDiskSize=0
+>>%ddf% echo/.set MaxErrors=1
+>>%ddf% echo/.set RptFileName=nul
+>>%ddf% echo/.set UniqueFiles=ON
+setlocal enabledelayedexpansion
+pushd "%~dp1"
+for /f "tokens=* delims=" %%# in ('dir /a:-D/b/s "%~1"') do (
+ set "DDir=%%~dp#" &set "DDir=!DDir:%~1=!" &set "DDir=\%~nx1!DDir:~0,-1!"
+ set "ChangeDDir=1" &if !DDir!==!LastDir! set "ChangeDDir=" 
+ if defined ChangeDDir echo/.Set DestinationDir=!DDir!;>>%ddf%
+ echo/"%%~f#"  /inf=no;>>%ddf%
+ set "LastDir=!DDir!"
+)
+makecab.exe /F %ddf% /D DiskDirectory1="" /D CabinetNameTemplate=%~nx1.cab &endlocal &popd &del /q /f %ddf% &echo. &exit/b
 :res85_encoder */
 //read
 arg0=WScript.Arguments(0), fn=arg0.replace(/_RSPACE_/g,' '), name=fn.split('\\').pop().split('/').pop(), exp=fn.slice(-1);
-WScript.Echo(' RES2BATCH: encoding '+fn);
+WScript.Echo('FILE2BACH: encoding '+fn+' ...');
 xe=WSH.CreateObject('Microsoft.XMLDOM').createElement('bh');rs=ws=WSH.CreateObject('ADODB.Stream');
 rs.Mode=3;rs.Type=1;rs.Open();rs.LoadFromFile(fn);len=rs.Size;xe.dataType='bin.hex';xe.nodeTypedValue = rs.Read();rs.Close();
 //encode
@@ -36,21 +57,18 @@ for(var l=a.length;l--;){var n=parseInt(a[l],16);a[l]='';for(j=5;j--;){a[l]=r85[
 var res85enc = pad>0? a.join('').slice(0,-pad):a.join('');
 //generate res85_decoder
 ws.Mode=3;ws.Type=2;ws.Charset='Windows-1252';ws.Open();
-ws.WriteText('/* 2>nul & TITLE Res2Batch by AveYo v2.0a\n@echo off &pushd %~dp0 &cscript.exe //nologo //e:JScript "%~f0"\n');
-if (exp==='_') ws.WriteText('set "res='+name+'"\nexpand -R "%res%" >nul 2>&1 &del /f /q "%res%"\n');
-ws.WriteText('pause &exit /b\n:res85_decoder */\nvar fn="'+name+'", res="\\\n');
+ws.WriteText('@set @v=3 /* Attach2Text\n@echo off &set "res='+name+'"\n');
+ws.WriteText('pushd %~dp0 &cscript.exe //nologo //e:JScript "%~f0" "%res%"\n');
+ws.WriteText('expand.exe -R "%res%" -F:* . >nul 2>nul &del /f /q "%res%" &exit/b\n');
+ws.WriteText(':res85_decoder */\nvar fn=WSH.Arguments(0); WSH.Echo("FILE2BACH: decoding "+fn+" ..."); var res="\\\n');
 var o=res85enc.match(/.{1,126}/g);for(i=0,l=o.length;i<l;i++) ws.WriteText("::"+o[i]+"\\\n");
 _dec="\".replace(\/[\\\\:\\s]\/gm,\"\"); \
 r85='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?.,;-_+=|{}[]()*^%$#!`~'.split('');\n\
 d85={}; for(var i=85;i--;) d85[r85[i]]=i; p85=[]; for(var i=5;i--;) p85[i]=Math.pow(85,i);\n\
 z='00000000'; pad=(res.length%5)||5; res+='~~~~~'.slice(pad); pad=10-2*pad; a=res.match(\/.{1,5}\/g);\n\
 for(var l=a.length;l--;){n=0;for(j=5;j--;)n+=d85[a[l].charAt(j)]*p85[4-j];a[l]=z.slice(n.toString(16).length)+n.toString(16)};\n\
-res85dec=(pad>0)?a.join('').slice(0,-pad):a.join('');WSH.Echo(' RES2BATCH: extracting '+fn);\n\
+res85dec=(pad>0)?a.join('').slice(0,-pad):a.join('');\n\
 xe=WSH.CreateObject('Microsoft.XMLDOM').createElement('bh');as=WSH.CreateObject('ADODB.Stream');as.Mode=3;as.Type=1;as.Open();\n\
 xe.dataType='bin.hex';xe.text=res85dec;as.Write(xe.nodeTypedValue);as.SaveToFile(fn,2);as.Close();\n";
 ws.WriteText(_dec);ws.SaveToFile(fn+'.bat',2);ws.Close();
-
-
-
-
-
+ 
