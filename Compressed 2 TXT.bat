1c8c27091d0e32ae401329de1af5aa7a19229099
AveYo <aveyo@yahoo.com>
2019-03-03 00:19:42 +0000

    Update Compressed 2 TXT.bat
    Windows 7 support ( PowerShell 2.0 / C# 2.0 )
    Very fast encoding and decoding BAT85 class
    Multiple file(s) and folder(s) "Send to" selection
    Optional line split and prefix

--- Compressed 2 TXT.bat
+++ Compressed 2 TXT.bat
@@ -1,24 +1,29 @@
-<# : Compressed 2 TXT : Efficient file / folder encoder by AveYo - output can be renamed to .bat or .ps1 to decode
-@echo off &title %~n0 v4.0 [%1]
-if not .%1==. goto :Encode
-echo. &echo  No input file or folder to encode! use 'Send to' context menu ...
-copy /y "%~f0" "%APPDATA%\Microsoft\Windows\SendTo\Compressed 2 TXT.bat" >nul 2>nul &goto :End
-:Encode
-pushd %~dp1 &set "InputFile=yes" &for /f "tokens=1 delims=r-" %%# in ("%~a1") do if /i ".%%#"==".d" set "InputFile="
-if defined InputFile ( set "cabfile=%~dpn1~" ) else set "cabfile=%~dpnx1~"
-call :MakeCab "%~f1" &if not exist "%cabfile%" echo  makecab '%~nx1' failed, try again ... &goto :End
-for %%# in ("%cabfile%") do if 1%%~z# GTR 220971520 echo  Input '%~nx1' is larger than the 20MB limit! &goto :End
-::rem comment above line to remove 20MB file limit
-type "%~f0" | powershell.exe -c -
-if exist "%cabfile%.bat" del /f /q "%cabfile%"
-timeout /t 10
-::start "preview" notepad "%cabfile%.bat.txt"
-exit/b
+@echo off & set "nr=" & set "id=" & title Compressed2TXT v5.0 &rem File(s)/Folder(s) "Send to" .bat ascii encoder by AveYo
+set/a USE_LINES=1
+set/a USE_PREFIX=0
+if not %1.==. goto :CompressAll
+color 0e & echo. & echo  No input file^(s^) or folder^(s^) to encode! use 'Send to' context menu ...
+copy /y "%~f0" "%APPDATA%\Microsoft\Windows\SendTo\Compressed 2 TXT.bat" >nul 2>nul & goto :End
+:CompressAll
+set "USE_LINES=%USE_LINES:1=$true%" & set "USE_LINES=%USE_LINES:0=$false%"
+set "USE_PREFIX=%USE_PREFIX:1=$true%" & set "USE_PREFIX=%USE_PREFIX:0=$false%"
+set/a nr=0 & set/a count=0 & for %%# in (%*) do set/a count+=1
+for %%# in (%*) do set/a nr+=1 & call :CompressOne "%%~#"
+goto :End
+:CompressOne
+pushd %~dp1 & set "IsFile=yes" & for /f "tokens=1 delims=r-" %%# in ("%~a1") do if /i ".%%#"==".d" set "IsFile="
+if defined IsFile ( set "cabfile=%~n1~" ) else set "cabfile=%~nx1~"
+call :MakeCab "%~f1" & if not exist "%cabfile%" echo MAKECAB '%~nx1' failed, try again ... &goto :End
+if not defined id call set "id=%~n1~"
+if not defined count set/a nr=1 & set/a count=1
+set "PSARGS=$nr=%nr%;$count=%count%;$id='%id%';$fn='%cabfile%'; $uselines=%USE_LINES%; $useprefix=%USE_PREFIX%;"
+powershell -noprofile -c "$f=[io.file]::ReadAllText('%~f0') -split ':CompressPS\:.*'; %PSARGS%; iex ($f[1]);"
+del /f /q "%cabfile%" & exit/b
 :End
-echo. &pause &title %comspec% &exit/b
-:MakeCab %1:[file or directory]
-echo  makecab '%~nx1' ... &echo.
-if defined InputFile makecab.exe /D CompressionType=LZX /D CompressionLevel=7 /D CompressionMemory=21 "%~nx1" "%~n1~" &exit/b
+echo. & pause & color 07 & title %comspec% & exit/b
+:MakeCab [file or directory]
+echo MAKECAB '%~nx1' ... &echo.
+if defined IsFile makecab.exe /D CompressionType=LZX /D CompressionLevel=7 /D CompressionMemory=21 "%~nx1" "%~n1~" &exit/b
 set ddf="%temp%\ddf"
  >%ddf% echo/.New Cabinet
 >>%ddf% echo/.set Cabinet=ON
@@ -48,80 +53,64 @@ for /f "tokens=* delims=" %%# in ('dir /a:-D/b/s "%~1"') do (
 )
 makecab.exe /F %ddf% /D DiskDirectory1="" /D CabinetNameTemplate="%~nx1~" &endlocal &popd &del /q /f %ddf% &echo. &exit/b
 
-:: PowerShell functions below this line #>
-function r85encode { param([Byte[]] $bin, [Byte] $split=0)
-  $xe = (new-object -COM Microsoft.XMLDOM).createElement("bh")
-  $xe.dataType = "bin.hex"
-  $xe.nodeTypedValue = [System.BitConverter]::ToString($bin).replace("-", "")
-  $r = ([regex]::Matches($xe.text, "(.{1,8})")).value
-  $pad = 8 - $r[-1].length
-  if($pad){ $r[-1] = $r[-1] + "00000000".Remove(0, 8 - $pad) }
-  $r85 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#&()*+,-./;=?@[]^_{|}~'
-  $d85 = @{}; for($i=85;$i--;){ $d85[$r85[$i]] = $i }
-  for($l=$r.length;$l--;){
-    $n = [uint64]::Parse($r[$l], "HexNumber")
-    $r[$l] = ""
-    for($j=5;$j--;){
-      $k = 0;
-      if($n -gt 0){ $k = [math]::Truncate($n % 85) }
-      $r[$l] = $r85[$k] + $r[$l]
-      $n /= 85
+:CompressPS: powershell / C# 2.0 functions
+Add-Type -Language CSharp -TypeDefinition @"
+using System.IO; using System.Text;
+public class BAT85 {
+  private static string a85="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#&()*+,-./;=?@[]^_{|}~";
+  private static byte[] b85=new byte[85]; private static long n=0; private static int[] p85={52200625,614125,7225,85,1};
+  private static byte[] n4b(){ return new byte[4]{(byte)(n>>24),(byte)(n>>16),(byte)(n>>8),(byte)n}; }
+  private static byte[] n5b(){ byte[] k;k=new byte[5]; for(int j=0;j<5;j++){ k[4-j]=b85[(byte)(n % 85)]; n /= 85; } return k;}
+  public static void Encode(string filename, string batname, bool bLines, bool bPrefix) {
+    int l=0; int p=0; MemoryStream ms=new MemoryStream(); n = 0; for(byte i=0;i<85;i++){b85[i]=(byte)a85[i];}
+    byte[] SOL=new byte[2]{0x3A,0x3A}; byte[] EOL=new byte[1]{0xA};
+		foreach (byte b in File.ReadAllBytes(filename)){
+      if (bLines) { if (bPrefix && l == 1) {ms.Write(SOL, 0, 2);} if (l == 101) {ms.Write(EOL, 0, 1); l = 0; } l++; }
+			if (p == 3) { n |= b; ms.Write(n5b(), 0, 5); n = 0; p = 0; } else { n |= (uint)(b << (24 - (p * 8))); p++; }
+      if (bLines && n == 0 && l > 99) {ms.Write(EOL, 0, 1); l = 0;}
+		} // done byte array loop, pad bytes and save:
+    if(p>0){ for(int i=p;i<3-p;i++){ n |= (uint)(0 << (24 - (p * 8))); } n |= 0; ms.Write(n5b(), 0, p + 1); }
+    byte[] marker = Encoding.UTF8.GetBytes("\r\n:" + "bat2file" + ": " + filename + "\r\n");
+    using (FileStream fs = new FileStream(batname, FileMode.Append)){
+     fs.Write(marker, 0, marker.Length); fs.Write(ms.ToArray(), 0, (int)ms.Length); ms.SetLength(0);
     }
   }
-  if($pad){ $r[-1] = $r[-1].Remove(5 - $pad / 2) }
-  if($split){
-    $s=([regex]::Matches($r -join '', "(.{1,"+$split+"})")).value
-    $s[0] = "::" + $s[0]
-    return $s -join "`r`n::"  # signature prefix all lines
+  public static void Decode(string fname, string s) {
+    MemoryStream ms=new MemoryStream(); n=0; for(byte i=0;i<85;i++){ b85[(byte)a85[i]]=i; }
+    bool k=false; int p=0;
+    foreach(char c in s){
+      switch(c){case'\0':case'\n':case'\r':case'\b':case'\t':case'\xA0':case' ':case':': k=false;break; default: k=true;break;}
+      if(k){ n+= b85[ (byte)c ] * p85[ p++ ]; if(p == 5){ms.Write(n4b(), 0, 4); n=0; p=0; } }
+    } // done char loop, pad bytes and save:
+    if(p>0){ for(int i=0;i<5-p;i++){ n += 84 * p85[ p+i ]; } ms.Write(n4b(), 0, p-1); }
+    File.WriteAllBytes(fname, ms.ToArray()); ms.SetLength(0);
   }
-  $r[0] = "::" + $r[0] # signature prefix
-  return $r -join ""
 }
-function r85decode { param([string] $str)
-  $r = ([regex]::Matches($str, '([^:\s]{1,5})')).value
-  $pad = 5 - $r[-1].length
-  if($pad){ $r[-1] = $r[-1] + "~~~~~".Remove($pad) }
-  $r85 = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#&()*+,-./;=?@[]^_{|}~'
-  $d85 = @{}; for($i=85;$i--;){ $d85[$r85[$i]] = $i }; $p85 = @(1,85,7225,614125,52200625)
-  for($l=$r.length;$l--;){
-    $n = 0
-    for($j=5;$j--;){
-      $n += $d85[$r[$l][$j]] * $p85[4 - $j]
-    }
-    $k = [Convert]::ToString($n, 16)
-    $r[$l] = "00000000".Remove(0, $k.length) + $k
-  }
-  if($pad){ $r[-1] = $r[-1].Remove(8 - $pad * 2) }
-  $xe = (new-object -COM Microsoft.XMLDOM).createElement("bh")
-  $xe.dataType = "bin.hex"
-  $xe.text = $r -join ""
-  return $xe.nodeTypedValue
-}
+"@;
+
+$EXPANDER = @'
+@echo off & pushd %~dp0
+powershell -noprofile -c "$f=[io.file]::ReadAllText('%~f0') -split ':bat2file\:.*';iex ($f[1]);
+'@;
+if ($count -gt 0){ for ($i=1;$i -le $count;$i++) { $EXPANDER+="X $i;" } }
+$EXPANDER += "`"`r`nexit/b`r`n`r`n"
+$EXPANDER += @'
+:bat2file: Compressed2TXT v5
+Add-Type -Language CSharp -TypeDefinition @"
+ using System.IO; public class BAT85{ public static void Decode(string tmp, string s) { MemoryStream ms=new MemoryStream(); n=0;
+ byte[] b85=new byte[255]; string a85="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#&()*+,-./;=?@[]^_{|}~";
+ int[] p85={52200625,614125,7225,85,1}; for(byte i=0;i<85;i++){b85[(byte)a85[i]]=i;} bool k=false;int p=0; foreach(char c in s){
+ switch(c){ case'\0':case'\n':case'\r':case'\b':case'\t':case'\xA0':case' ':case':': k=false;break; default: k=true;break; }
+ if(k){ n+= b85[(byte)c] * p85[p++]; if(p == 5){ ms.Write(n4b(), 0, 4); n=0; p=0; } } }         if(p>0){ for(int i=0;i<5-p;i++){
+ n += 84 * p85[p+i]; } ms.Write(n4b(), 0, p-1); } File.WriteAllBytes(tmp, ms.ToArray()); ms.SetLength(0); }
+ private static byte[] n4b(){ return new byte[4]{(byte)(n>>24),(byte)(n>>16),(byte)(n>>8),(byte)n}; } private static long n=0; }
+"@; function X([int]$r=1){ $tmp="$r._"; echo "`n$r.."; [BAT85]::Decode($tmp, $f[$r+1]); expand -R $tmp -F:* .; del $tmp -force }
+'@;
+
+if ($nr -eq 1){[System.IO.File]::WriteAllLines($id+'.bat', $EXPANDER)}
 echo " "
-echo " r85encode input cabfile ..."
-$cabfile=([System.IO.FileInfo]$env:cabfile).BaseName
-$bin = [System.IO.File]::ReadAllBytes($cabfile);
-$content = r85encode $bin 125 # to also limit encode output line length use: r85encode $bin 125
-if ($content.length -lt 5){ throw "File2TXTenc failed" }
-
-echo " Generate output file with bundled r85decode ..."
-$outfile=$cabfile+'.bat';
-$hd = @()
-$hd+='<# : Compressed 2 txt : Efficient file / folder encoder by AveYo - can rename to .bat or .ps1 to decode'
-$hd+='@powershell -NoProfile -NoLogo -Command "iex (${%~f0} | out-string)" &exit/b'
-$hd+='#> $file="'+$cabfile+'"; write-host "`r`n Compressed 2 txt : r85decode $file ..."; $res=@"'
-#$content
-$ft = @()
-$ft+='"@; $r=([regex]::Matches($res,''([^:\s]{1,5})'')).value;$pad=5-$r[-1].length;if($pad){$r[-1]=$r[-1]+"~~~~~".Remove($pad)}'
-$ft+='$r85=''0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#&()*+,-./;=?@[]^_{|}~'';'
-$ft+='$d85=@{}; for($i=85;$i--;){$d85[$r85[$i]]=$i}; $p85=@(1,85,7225,614125,52200625);  for($l=$r.length;$l--;){'
-$ft+='$n=0; for($j=5;$j--;){$n += $d85[$r[$l][$j]] * $p85[4 - $j]}; $k=[Convert]::ToString($n,16);'
-$ft+='$r[$l]="00000000".Remove(0,$k.length)+$k  };  if($pad){$r[-1]=$r[-1].Remove(8-$pad*2)}'
-$ft+=''
-$ft+='$xe=(new-object -COM Microsoft.XMLDOM).createElement("bh");  $xe.dataType="bin.hex";  $xe.text=$r -join ""'
-$ft+='[IO.File]::WriteAllBytes($file,$xe.nodeTypedValue);  expand -R $file -F:* .;  del $file -force;  timeout /t 10 2>$nul'
-[IO.File]::WriteAllLines($outfile,$hd);
-[IO.File]::AppendAllLines([string]$outfile,[string[]]$content);
-[IO.File]::AppendAllLines([string]$outfile,[string[]]$ft);
-
-echo " Done"
+echo "BAT85 encoding $fn ..."
+[BAT85]::Encode($fn, $id+'.bat', $uselines, $useprefix);
+echo "DONE!"
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+:CompressPS: end
